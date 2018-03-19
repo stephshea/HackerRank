beeca69a89ebfa3e57fa2672ed654bc8031de998
Stephanie Shea <stephcshea@gmail.com>
2018-03-19 14:59:45 +0000

    day 0 & day 1 solutions

---
+++ Day0.js
@@ -0,0 +1,6 @@
+function processData(inputString) {
+	// This line of code prints the first line of output
+	// Write the second line of output that prints the contents of 'inputString' here.
+	console.log(inputString);
+}
+console.log("Hello, World.");
---
+++ Day1.js
@@ -0,0 +1,12 @@
+  // Declare second integer, double, and String variables.
+  var second = readLine();
+  var double = readLine();
+  var string = readLine();
+  // Read and save an integer, double, and String to your variables.
+  console.log(i + parseInt(second));
+  console.log((parseFloat(double) + d).toFixed(1));
+  console.log(s + string);
+  // Print the sum of both integer variables on a new line.
+  // Print the sum of the double variables on a new line.
+  // Concatenate and print the String variables on a new line
+  // The 's' variable above should be printed first.
